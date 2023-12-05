8ae5e78f5e90bd8adf4b6979ff0fcc9cc727f025
jaydipgohilpel <jaydip.gohil@proexelancers.com>
2023-12-05 15:43:14 +0530

    create navbar

--- e-dashboard-web/src/app/app-routing.module.ts
+++ e-dashboard-web/src/app/app-routing.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddProductComponent } from './add-product/add-product.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { UpdateProductComponent } from './update-product/update-product.component';
+import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
   { path: '', component: AddProductComponent },
+  { path: 'products', component: ProductListComponent },
+  { path: 'add-products', component: AddProductComponent },
+  { path: 'update-product', component: UpdateProductComponent },
+  { path: 'profile', component: ProfileComponent },
 ];
 
 @NgModule({
