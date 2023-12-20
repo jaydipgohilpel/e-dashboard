fe2aaa9444e0e837560f64c24b7d43f1e338ec01
jaydipgohilpel <jaydip.gohil@proexelancers.com>
2023-12-20 16:07:36 +0530

    add login api, add jwt token ,generate token in login api

--- backend/index.js
+++ backend/index.js
@@ -4,29 +4,39 @@ const cors = require('cors')
 require('./db/config.js')
 const User = require('./db/User.js')
 const app = express();
+const jwt = require('jsonwebtoken');
+const { secret } = require('./secret/secretKey.js');
 
 app.use(express.json())
 app.use(cors());
 
 app.post('/register', async (req, res) => {
     const user = new User(req.body)
-    const result = await user.save()
+    let result = await user.save()
     result = result.toObject();
     delete result.password;
-    res.status(200).json({ data: result, code: 200 });
+    res.status(200).json({ data: result, code: 200, success: true });
 })
 
 app.post('/login', async (req, res) => {
     if (req.body.email && req.body.password) {
-        let user = await User.findOne(req.body).select("-password")
+        const user = await User.findOne({ email: req.body.email })
         if (user) {
-            res.status(200).json({ data: req.body, code: 200 });
+            if (user.password === req.body.password) {
+                let result = user.toObject();
+                delete result.password;
+                const token = jwt.sign(result, secret, { expiresIn: '24h' });
+                res.status(200).json({ token: token, code: 200, success: true });
+            }
+            else {
+                res.status(200).json({ error: 'Invalid Email or Password', code: 200, success: false, });
+            }
         } else {
-            res.status(404).json({ data: 'No User found', code: 404 });
+            res.status(404).json({ error: 'User not Exist', code: 404, success: false });
         }
     }
     else {
-        res.status(400).json({ data: 'Email or Password not provided', code: 400 });
+        res.status(400).json({ error: 'Email or Password not provided', code: 400, success: false });
     }
 })
 app.listen(4000)
